9769e0133c44d5487b10f207285f95edad581689
freelancernoor <info@devnurul.me>
2021-09-27 01:54:30 +0600

    react revisions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react/cjs/react.development';
 
 function App() {
+  const hero =[
+    "Nurul", "Ayman", "Sadman"
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieCounter></MovieCounter>
+      <Hero name = {hero[0]} age="59"></Hero>
+      <Hero name = {hero[1]}></Hero>
+      <Hero name = {hero[2]}></Hero>
     </div>
   );
+}
+function MovieCounter(props){
+
+ const result = useState(0);
+  return (
+    <div>
+      <button> Add movies</button>
+      <h5>Number of movies :</h5>
+    </div>
+  )
+}
+
+
+
+function Hero(props){
+  return(<div style ={{border:"2px solid #ddd", width:"350px", margin:"20px auto"}}>
+    <h3 style= {{fontSize:"25px"}}>I am " {props.name}"</h3>
+    <h3>I am {props.age || 5} year old</h3>
+  </div>)
 }
 
 export default App;
